bf633dcaf49444afd35043e157c205e0f6530e2f
LinuxUser404 <nikolay.pershyn@gmail.com>
2017-10-10 16:54:33 -0600

    more comments and refactoring

--- src/haskell-mcint.hs
+++ src/haskell-mcint.hs
@@ -23,7 +23,7 @@ main = do
   hPutStrLn stdout $ "There are " ++ ((show . length) (platforms)) ++ " OpenCL platforms."
   vendor  <- ((oclPlatformInfo platforms) !! platformNumber)
   hPutStrLn stdout $ "Selected platform: [" ++ (show platformNumber) ++ "]" ++ vendor
-  (device:_) <- clGetDeviceIDs (platforms!!platformNumber) CL_DEVICE_TYPE_GPU -- possible options: CPU, GPU, ALL, DEFAULT, ACCELERATOR
+  device <- head `fmap` clGetDeviceIDs (platforms!!platformNumber) CL_DEVICE_TYPE_GPU -- possible options: CPU, GPU, ALL, DEFAULT, ACCELERATOR
   deviceName <- clGetDeviceName device
   hPutStrLn stdout $ "Selected device: " ++ deviceName -- in my case Quadro K4200
   
@@ -31,21 +31,26 @@ main = do
   hPutStrLn stdout ""
   hPutStrLn stdout $ "Using kernel:\n" ++ clText
   hPutStrLn stdout ""
-  context <- clCreateContext [CL_CONTEXT_PLATFORM (platforms!!platformNumber)] [device] print -- uses print to display text output(if something goes wrong)
-  q <- clCreateCommandQueue context device [] -- creates command queue with default properties []
+  
+  -- create context and compile OpenCL program
+  context <- clCreateContext [CL_CONTEXT_PLATFORM (platforms!!platformNumber)] [device] $ hPutStrLn stdout -- uses "hPutStrLn stdout" to display text output(if something goes wrong)
   program <- clCreateProgramWithSource context clText -- creates OpenCL program from the kernel source
   clBuildProgram program [device] "" -- compiles the program without any specific optimization options ""
-  kernel0 <- clCreateKernel program functionName -- function name in the source
-
+  kernel <- clCreateKernel program functionName -- function name in the source
+  
   input <- newArray inputData
   mem_in <- clCreateBuffer context [CL_MEM_READ_ONLY, CL_MEM_COPY_HOST_PTR] (vecSize, castPtr input)  
   mem_out <- clCreateBuffer context [CL_MEM_WRITE_ONLY] (vecSize, nullPtr)
-  clSetKernelArgSto kernel0 0 mem_in  -- storable data 0
-  clSetKernelArgSto kernel0 1 mem_out -- storable data 1
-  eventExec <- clEnqueueNDRangeKernel q kernel0 [length inputData] [] []
-  eventRead <- clEnqueueReadBuffer q mem_out True 0 vecSize (castPtr input) [eventExec]
+  clSetKernelArgSto kernel 0 mem_in  -- storable data 0
+  clSetKernelArgSto kernel 1 mem_out -- storable data 1
   
+  -- create command queue and queue the execution on a device
+  q <- clCreateCommandQueue context device [] -- creates command queue with default properties []
+  eventExec <- clEnqueueNDRangeKernel q kernel [length inputData] [] []  -- queue the execution
+  eventRead <- clEnqueueReadBuffer q mem_out True 0 vecSize (castPtr input) [eventExec] -- read the result once execution is complete
   outputData <- peekArray (length inputData) input
+  
+  
   hPutStrLn stdout $ "Input  array = " ++ show inputData
   hPutStrLn stdout $ "Output array = " ++ show outputData
   return()
@@ -64,9 +69,9 @@ oclPlatformInfo = map (\pid -> clGetPlatformInfo pid CL_PLATFORM_VENDOR)
 testFunction = FunctionExpression "multiplyBy2" "2 * x1" ["x1"]
 
 -- the program should take something like this as an input and produce the result
-testInput = "Integrate[1/(x^3 + 1), {x, 0, 1}]"
-testNumOutput = showCReal 100 $ (1/18) * (2 * sqrt(3) * pi + log(64)) -- the exact number with 100 digits precision
-testOutPut = "0.8356488482647210533371034597001107667865221274843319432301883149605056010320161997633294384028262855" -- the value of testNumOutput
+testInput = "Integrate[1/(x^3 + 1)/(y^3 + 1), {x, 0, 1}, {y, 0, 1}]"
+testNumOutput = showCReal 100 $ ((1/18) * (2 * sqrt(3) * pi + log(64))) ** 2 -- the exact number with 100 digits precision
+testOutPut = "0.6983089976061547905950713595903295502322592708600975842346346477469051938999891540922414594979416232" -- the value of testNumOutput
 
 
 {-
