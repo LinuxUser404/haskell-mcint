dc1acd63cb6a88affda952bf0bc329c1bd3cf5c3
LinuxUser404 <nikolay.pershyn@gmail.com>
2017-10-11 01:16:35 -0600

    basic steps towards multivariable functions

--- src/KernelGen.hs
+++ src/KernelGen.hs
@@ -11,12 +11,19 @@ import FunctionTypes
 
 genKernel :: FunctionExpression -> String
 genKernel expr = "__kernel " ++ "void " ++ (name expr)
-               ++ "(" ++ (intercalate ", " (args `mappend` (outarg:[])) ) ++ ")"
+               ++ "(" ++ (intercalate ", " (outarg:args) ) ++ ")"
                ++ "{"
                ++ "int id = get_global_id(0);"
-               ++ "out[id] = " ++ (expression expr) ++ "[id]" ++ ";"
+               ++ "out[id] = " ++ fexpr ++ ";"
                ++ "}"
   where
     args   = map ((++) "__global float *") (variables expr) :: [String]
     outarg = "__global float *out"
+    fexpr = substitute (expression expr) (variables expr) "[id]"
     
+
+
+substitute :: String -> [String] -> String -> String
+substitute [] _ _ = []
+substitute ('#':st) vars ids = ids ++ (substitute st vars ids)
+substitute expr vars ids = (takeWhile (/='#') expr) ++ (substitute (dropWhile (/='#') expr) vars ids)
--- src/haskell-mcint.hs
+++ src/haskell-mcint.hs
@@ -12,6 +12,7 @@ import Foreign.Marshal.Array( newArray, peekArray )
 import Foreign.C.Types( CFloat )
 
 import Data.Number.CReal( showCReal )  -- arbitrary precision real numbers
+import Data.List
 
 import KernelGen ( genKernel )         -- module that generates OpenCL kernels, aka content of a .cl file
 import FunctionTypes
@@ -38,28 +39,36 @@ main = do
   clBuildProgram program [device] "" -- compiles the program without any specific optimization options ""
   kernel <- clCreateKernel program functionName -- function name in the source
   
-  input <- newArray inputData
-  mem_in <- clCreateBuffer context [CL_MEM_READ_ONLY, CL_MEM_COPY_HOST_PTR] (vecSize, castPtr input)  
+  x1s <- newArray x1sData
+  x2s <- newArray x2sData
+  mem_x1s <- clCreateBuffer context [CL_MEM_READ_ONLY, CL_MEM_COPY_HOST_PTR] (vecSize, castPtr x1s)  
+  mem_x2s <- clCreateBuffer context [CL_MEM_READ_ONLY, CL_MEM_COPY_HOST_PTR] (vecSize, castPtr x2s)  
   mem_out <- clCreateBuffer context [CL_MEM_WRITE_ONLY] (vecSize, nullPtr)
-  clSetKernelArgSto kernel 0 mem_in  -- storable data 0
-  clSetKernelArgSto kernel 1 mem_out -- storable data 1
+  clSetKernelArgSto kernel 0 mem_out  -- kernel's 0th agrument
+  clSetKernelArgSto kernel 1 mem_x1s  -- kernel's 1st agrument
+  clSetKernelArgSto kernel 2 mem_x2s  -- kernel's 2nd agrument
   
   -- create command queue and queue the execution on a device
   q <- clCreateCommandQueue context device [] -- creates command queue with default properties []
-  eventExec <- clEnqueueNDRangeKernel q kernel [length inputData] [] []  -- queue the execution
-  eventRead <- clEnqueueReadBuffer q mem_out True 0 vecSize (castPtr input) [eventExec] -- read the result once execution is complete
-  outputData <- peekArray (length inputData) input
+  eventExec <- clEnqueueNDRangeKernel q kernel [dataLength] [] []  -- queue the execution
+  eventRead <- clEnqueueReadBuffer q mem_out True 0 vecSize (castPtr x1s) [eventExec] -- read the result once execution is complete
+  outputData <- peekArray dataLength x1s
   
   
-  hPutStrLn stdout $ "Input  array = " ++ show inputData
+  hPutStrLn stdout $ "Input array1 = " ++ show x1sData
+  hPutStrLn stdout $ "Input array2 = " ++ show x2sData
   hPutStrLn stdout $ "Output array = " ++ show outputData
   return()
     where
       platformNumber = 0  -- using the first platform
-      vecSize = (length inputData) * (sizeOf (0 :: CFloat))
-      inputData = 0.2:17.0:0.33:11:[] :: [CFloat]
+      vecSize = dataLength * (sizeOf (0 :: CFloat))
+      xsData = (take n [[x,x*2] | x <- [1..]]) :: [[CFloat]]
+      x1sData = (transpose xsData)!!0
+      x2sData = (transpose xsData)!!1
+      dataLength = length xsData
       clText = genKernel testFunction
       functionName = name testFunction
+      n = 10
 
 
 -- replaces list of platfrom IDs with their vendors
@@ -67,7 +76,7 @@ oclPlatformInfo :: [CLPlatformID] -> [IO String]
 oclPlatformInfo = map (\pid -> clGetPlatformInfo pid CL_PLATFORM_VENDOR)
 
 -- TODO: multivariable and position independent function
-testFunction = FunctionExpression "multiplyBy2" "2 * x1" ["x1"]
+testFunction = FunctionExpression "myAdd" "x1# + x2#" ["x1","x2"]
 
 -- GOAL: the program should take something like this as an input and produce the result
 testInput = "Integrate[1/(x^3 + 1)/(y^3 + 1), {x, 0, 1}, {y, 0, 1}]"
