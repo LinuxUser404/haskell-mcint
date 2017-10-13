4d9e9eeaf7f6614895aa4aa0c5189b96e48d8dfd
LinuxUser404 <nikolay.pershyn@gmail.com>
2017-10-13 01:20:22 -0600

    added comments to FunctionParser

--- src/FunctionParser.hs
+++ src/FunctionParser.hs
@@ -11,7 +11,7 @@ import Text.ParserCombinators.Parsec
 import FunctionTypes
 
 {-
-Takes String and converts it to and object which
+Takes stream of chars and converts it(reads from it) to an object which
 contains the function and its limits of integration
 -}
 parseIntegrate :: GenParser Char st FunctionExpression
@@ -29,21 +29,22 @@ parseIntegrate = do
   spaces
   char ']'
   return $ FunctionExpression "Integrate" expr vars
-  
-
+  --example input: "Integrate[x1 * x2 + x2, {x1,0,10}, {x2,0,20}]"  
+  --example output: FunctionExpression "Integrate" "x1 * x2 + x2" [("x1", Limits 0.0 10.0),("x2", Limits 0.0 20.0)]
 
 parseExpression :: GenParser Char st String
 parseExpression = many (noneOf("[,{}]")) >>= \str -> return str
 
+-- parseVariable, if comma follows perform recursive call, otherwise return list with single element var
 parseVariables :: GenParser Char st [(String, Limits)]
 parseVariables = spaces >> parseVariable >>= \var -> (((char ',') >> (parseVariables >>= \vars -> return $ var:vars)) <|> (return $ var:[]))
-  
+
 
 parseVariable :: GenParser Char st (String, Limits)
 parseVariable = do
-  char '{'
-  spaces
-  str   <- many (noneOf(" ,"))
+  char '{'                     -- first character in the stream should be '{'
+  spaces                       -- then any number(maybe 0) of spaces follow
+  str   <- many (noneOf(" ,")) -- a string of characters - the variable name
   spaces
   char ','
   spaces
