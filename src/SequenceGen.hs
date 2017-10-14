e63017e8a7689f720595f359108580cca032ea8c
LinuxUser404 <nikolay.pershyn@gmail.com>
2017-10-13 23:50:48 -0600

    More comments

--- src/SequenceGen.hs
+++ src/SequenceGen.hs
@@ -77,8 +77,8 @@ l - the number specified above
 Direction numbers V have to be generated separately and depend on number of dimensions.
 Sobol supplied a table for these numbers and properties they have to satisfy(so called A and A').
 Unfortunately the numbers satisfying both properties are known maximum for 6 dimensions.
-Numbers satisfying just property A known maximum 16 dimensions.
-Numbers satisfying property A supplied by other authors know up to 21001 dimensions(S. Joe and F. Y. Kuo 2008).
+Numbers satisfying just property A known maximum for 16 dimensions.
+Numbers satisfying property A supplied by other authors known up to 21001 dimensions(S. Joe and F. Y. Kuo 2008).
 -}
 
 -- direction numbers V
